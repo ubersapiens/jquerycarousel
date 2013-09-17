6319b9eedd3c963a41b7cd5e67dfba443daee09a
JFP3 <jfpardini@gmail.com>
2013-09-17 10:41:58 +0200

    Test.js branching.

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -1,41 +1,18 @@
-// (function(){
-// 
-//   $.fn.juanCarousel = function(options){
-//     return this.each(function(){
-//       var carousel = new JuanCarousel(this, options);
-//     });
-//   };
-// 
-//   function JuanCarousel(element, options){
-//     this.element = element;
-//     this.options = options;
-// 
-//     this.load_photos(function(){
-//       this.display_photos();
-//     });
-//   };
-// 
-//   JuanCarousel.prototype.load_photos = function(callback){
-//     var instance = this;
-// 
-//     $.ajax({
-//       url: '/photos.json',
-//       type: 'GET',
-//       dataType: 'json',
-//       success: function(response){
-//         instance.photos = response.photos;
-//         callback.apply(instance);
-//       }
-//     });
-// 
-//   };
-// 
-//   JuanCarousel.prototype.display_photos = function(){
-//     for (var i=0; i < this.photos.length; i++) {
-//       img = this.photos[i];
-//       $('.carousel .image_container').html('<img src="' + img.src + '" />');
-//       $('.carousel .text_container').html(img.text);			
-//     }
-//   };
-// 
-// })();
+(function(){
+
+  $.fn.juanCarousel = function(options){
+    return this.each(function(){
+      var carousel = new JuanCarousel(this, options);
+    });
+  };
+
+  function JuanCarousel(element, options){
+    this.element = element;
+    this.options = options;
+
+    this.load_photos(function(){
+      this.display_photos();
+    });
+  };
+
+})();
