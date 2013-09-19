af5ab4973042c424ea8d80cc0384621a19c776d8
JFP3 <jfpardini@gmail.com>
2013-09-19 10:25:10 +0200

    Next, previous and stop buttons work. Play missing.

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,11 +1,9 @@
+var interval = null;
 $(document).ready(function(){
 	
 	$('.carousel').juanCarousel({
 		url: '/photos.json',
 		speed: '', // option to change the speed,
-		onChange: function(photos){
-			
-		}
 	});
 	
 });
--- public/javascript/juan_carousel.jquery.js
+++ public/javascript/juan_carousel.jquery.js
@@ -1,8 +1,10 @@
 (function(){
 
 	$.fn.juanCarousel = function(options){
-		return this.each(function(){
-			var carousel = new JuanCarousel(this, options);  
+		
+		//The method .each needs to be placed into the function to maintain chainability
+		return this.each(function(){  
+			new JuanCarousel(this, options);  
 		});
 	};
 
@@ -10,7 +12,18 @@
 		this.element = $(element);
 		this.options = options;
 		this.current_index = 0;
+		this.setup();
 		this.load_photos();
+	};
+	
+	JuanCarousel.prototype.setup = function(){
+		this.element.append('<div class="image_container"></div>');
+		this.element.append('<div class="text_container"></div>');
+		this.element.append('<div class="controls_container"></div>');
+		this.element.append('<div class="controls_container"><img src="/images/play_icon.png" id="play_button" /> </div>');
+		this.element.append('<div class="controls_container"><img src="/images/stop_icon.jpg" id="pause_button" /> </div>');
+		this.element.append('<div class="controls_container"><img src="/images/back_icon.png" id="back_button" /> </div>');
+		this.element.append('<div class="controls_container"><img src="/images/forward_icon.png" id="forward_button" /> </div>');
 	};
 
 	JuanCarousel.prototype.load_photos = function(){
@@ -22,23 +35,44 @@
 			dataType: 'json',
 			success: function(response){
 				instance.photos = response.photos;
+				instance.next_photos();
+				instance.last_photos();
 				instance.show_photos();
-				instance.carousel_prev();
-				instance.carousel_next();
-				setInterval(function(){
+				$('#forward_button').click(function() {
+					instance.advance();
+				});
+				$('#back_button').click(function() {
+					instance.rewind();
+				});
+				var interval = setInterval(function(){
 					instance.advance.apply(instance);
 				}, 3000);
+				$('#pause_button').click(function () {
+					clearInterval(interval);
+				})
+				// $('#play_button').click(function () {
+				// 	console.log(this);
+				// 	setInterval(3000);
+				// })
 			}
 		});
 	}; 
 	
 	JuanCarousel.prototype.show_photos = function() {	
 		this.element.find('.image_container').html('<img src="' + this.photos[this.current_index].src + '" />');
+		this.element.find('.text_container').html('<p>' + this.photos[this.current_index].caption + ' </p>');
 	};
-
+	
 	JuanCarousel.prototype.next_photos = function(){
 		this.current_index = this.current_index + 1;
 		if (this.current_index > (this.photos.length -1)) {
+			this.current_index = 0;
+		}
+	};
+	
+	JuanCarousel.prototype.last_photos = function() {
+		this.current_index = this.current_index + 1;
+		if (this.current_index > (this.photos.length +1)) {
 			this.current_index = 0;
 		}
 	};
@@ -48,19 +82,10 @@
 		this.show_photos();
 	};	
 	
-	JuanCarousel.prototype.carousel_prev = function() {
-		console.log('Carousel prev loaded');
-		this.element.find('#carousel_prev').click(function(e) {
-			e.preventDefault();
-		});
+	JuanCarousel.prototype.rewind = function() {
+		this.last_photos();
+		this.show_photos();
 	};
-	
-	JuanCarousel.prototype.carousel_next = function() {
-		console.log('Carousel next loaded');
-	  this.element.find('#carousel_next').click(function(e) {
-	    e.preventDefault();
-	  });
-	};	
 	
 	
 })();
